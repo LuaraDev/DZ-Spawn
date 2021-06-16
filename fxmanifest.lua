ab580543719eedb8691643f0e16ed7da730d92e2
Luara <81599998+LuaraDev@users.noreply.github.com>
2021-06-16 17:12:37 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,6 @@ author 'dz-security.live'
 
 client_script 'client/cl_main.lua'
 client_script 'config.lua'
-server_script 'server/sv_main.lua'
-server_script 'config.lua'
-server_script '@mysql-async/lib/MySQL.lua'
 
 ui_page 'html/index.html'
 files {
