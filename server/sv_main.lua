087a8cef5fabdfd7092c54de76f5ef0bf1926783
Luara <81599998+LuaraDev@users.noreply.github.com>
2021-06-16 17:12:47 +0300

    Update sv_main.lua

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,16 +1,1 @@
--- https://dz-security.live
 
-RegisterServerEvent('dz-spawn:server:getdata')
-AddEventHandler('dz-spawn:server:getdata', function()
-    local src = source
-    MySQL.Async.fetchAll('SELECT * FROM users WHERE identifier = @id', {
-        ['@id'] = GetPlayerIdentifiers(src)[1]
-    }, function(data)
-        local global = json.decode(data[1]['position'])
-        local position = {}
-        position['x'] = global.x
-        position['y'] = global.y
-        position['z'] = global.z
-        TriggerClientEvent("dz-spawner:client:GetData", src, position)
-    end)
-end)
